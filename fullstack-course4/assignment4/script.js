8316eb16238fa495cd362be8108b418cb7ac068f
Trevor Merrifield <trevorm42@gmail.com>
2017-08-20 14:32:33 -0400

    Complete Assignment 4
    Steps 10, 11, 12

--- fullstack-course4/assignment4/script.js
+++ fullstack-course4/assignment4/script.js
@@ -43,7 +43,8 @@
   // using the 'speak' method or either helloSpeaker's or byeSpeaker's
   // 'speak' method.
   // See Lecture 50, part 1
-  for (/* fill in parts of the 'for' loop to loop over names array */) {
+  // DONE
+  for (var i = 0; i < names.length; i++) {
 
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
@@ -52,17 +53,19 @@
     // string object's 'toLowerCase' method on the result so we can compare
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
-    // var firstLetter =
+    // DONE
+    var firstLetter = names[i][0].toLowerCase();
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
     // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
-    if (/* fill in condition here */) {
-      // byeSpeaker.xxxx
+    // DONE
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
     } else {
-      // helloSpeaker.xxxx
+      helloSpeaker.speak(names[i]);
     }
   }
 })()
